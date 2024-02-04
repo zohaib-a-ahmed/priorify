29b93dda09f4a88214088b52e51c0ff879d7c454
Zohaib Ahmed <zohaib0718@gmail.com>
2024-02-04 15:41:04 -0600

    Mobile Support

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import { useEffect } from 'react';
 import { supabase } from '../app/supabase/client';
 import { useRouter } from 'next/navigation';
 import { Center, Title, Button, Text, Container, Paper } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 
 
 export default function Index() {
 
   const router = useRouter();
+  const isSmallScreen = useMediaQuery('(max-width: 768px)');
 
   async function handleSignInWithGoogle() {
     const { data, error } = await supabase.auth.signInWithOAuth({
@@ -30,13 +32,18 @@ export default function Index() {
 
   return (
     <div style={{ backgroundColor: '#333333', minHeight: '100vh' }}>
-        <Container>
-          <Center style={{ height: '100vh' }}>
-            <Paper shadow="lg" radius="md" withBorder p="xl">
-              <Center>
-                <Title fw={800} order={1} style={{ marginBottom : 10, color : '#F39C12'}}>Priorify</Title>             
-              </Center>
-              <Text fw={550}> LLM-Assisted Productivity Hub</Text> 
+      <Container>
+        <Center style={{ height: '100vh' }}>
+          <Paper shadow="lg" radius="md" withBorder p="xl">
+            <Center>
+              <Title fw={800} order={1} style={{ marginBottom: 10, color: '#F39C12' }}>Priorify</Title>             
+            </Center>
+            <Text fw={550}> LLM-Assisted Productivity Hub</Text>
+            {isSmallScreen ? (
+              <Text color="red" size="sm" mt='lg'>
+                Smaller screens are not yet supported.
+              </Text>
+            ) : (
               <Button
                 fullWidth
                 variant="gradient"
@@ -48,8 +55,9 @@ export default function Index() {
               >
                 Sign In With Google
               </Button>
-            </Paper>         
-          </Center>
+            )}
+          </Paper>         
+        </Center>
       </Container>      
     </div>
   );
